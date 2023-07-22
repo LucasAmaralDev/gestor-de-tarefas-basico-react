2955e5ce151348a8a65ce26c89d9c0ff8f775d09
Lucas Henrique <lucasahenriqueamaral@gmail.com>
2023-07-22 17:07:45 -0400

    otimizadas funcionalidades basicas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [tarefas, setTarefas] = useState([])
+
+  const [texto, setTexto] = useState('')
+
+
+  function adicionarTarefa() {
+    //caso nao tenha texto nao executa a funcao
+    if (!texto) return
+    //adicionar tarefa
+    setTarefas([...tarefas, { texto: texto, iscompleted: false }])
+  }
+
+  function tarefaConcluida(indice) {
+    const listaMarcada = tarefas.map((tarefa, index) => {
+      // se o indice indicado for igual ao indice da tarefa ela muda a marcacao booleana 
+      if (index === indice) {
+        tarefa.iscompleted = !tarefa.iscompleted
+        return tarefa
+      }
+      //senão só retorna o valor previo
+      return tarefa
+    })
+    //retorna a nova lista ao estado
+    setTarefas(listaMarcada)
+  }
+
+
+  function excluirTarefa(indice){
+    const novaLista = tarefas.filter((tarefa, index)=>{
+      if (index !== indice){
+        return tarefa
+      }
+
+      
+    } )
+    setTarefas(novaLista)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tarefas </h1>
+
+      <input type="text" onChange={(e) => { setTexto(e.target.value) }} value={texto} />
+      <button onClick={adicionarTarefa}>Adicionar</button>
+
+
+      {tarefas && tarefas.map((tarefa, index) => {
+        return (
+          <>
+            <p key={index}> {tarefa.texto} {tarefa.iscompleted ? <p>completa</p> : <p>Incompleta</p>}</p>
+            <button onClick={() => { tarefaConcluida(index) }}>Completar</button>
+            <button onClick={() => { excluirTarefa(index) }}>Excluir</button>
+          </>
+        )
+      })}
     </div>
   );
 }
